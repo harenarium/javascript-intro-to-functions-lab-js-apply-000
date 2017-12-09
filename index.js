1c5fc837d175b532f2986f222f30ba92bd5e7979
harenarium <carolinel2610@gmail.com>
2017-12-09 18:14:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,22 +16,23 @@ function logWhisper(string){
 
 function sayHiToGrandma(string){
 
-  switch(string){
-    case string.toLowerCase():
-        return "I can't hear you!"
-    case string.toUpperCase():
-        return "YES INDEED!"
-    case "I love you, Grandma.":
-        return "I love you, too."
-    default:
-        break;
+  if(string === string.toLowerCase()){
+    return "I can't hear you!"
+  } else if(string === string.toUpperCase()){
+    return "YES INDEED!"
+  } else if(string === "I love you, Grandma."){
+    return "I love you, too."
   }
-  //
-  // if(string === string.toLowerCase()){
-  //   return "I can't hear you!"
-  // } else if(string === string.toUpperCase()){
-  //   return "YES INDEED!"
-  // } else if(string === "I love you, Grandma."){
-  //   return "I love you, too."
+  // another way
+  // switch(string){
+  //   case string.toLowerCase():
+  //       return "I can't hear you!"
+  //   case string.toUpperCase():
+  //       return "YES INDEED!"
+  //   case "I love you, Grandma.":
+  //       return "I love you, too."
+  //   default:
+  //       break;
   // }
+
 }
